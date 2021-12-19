5ca47d3d3274c3ed79f1ceb44b62bc63a0db2ac2
cidaluna <cidalunna@hotmail.com>
2021-12-19 14:23:10 -0300

    criando tela bem vindos

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,14 +8,28 @@ import { HeaderComponent } from "./components/template/header/header.component";
 
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatCardModule} from '@angular/material/card';
+import { MatListModule} from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    AppComponent, 
+    HeaderComponent, 
+    FooterComponent, 
+    NavComponent, HomeComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatListModule,
+    MatSidenavModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent],
